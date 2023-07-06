628384de57ba61e6871e8c39f9a55957ae58e50c
felixchan <siuminghoho@hotmail.com>
2023-07-06 18:17:12 +0800

    ready to the game of life

---
+++ gol/gameOfLife.js
@@ -0,0 +1,18 @@
+//setup() function is for setting up the initial values. It is going to run exactly once.
+function setup() {
+
+
+
+
+}
+
+
+// draw() is going to run many times. It is going to run once per frame. If you have a frame rate of 30 frames per second, then draw() runs 30 times per second!
+
+function draw() {
+
+
+
+
+
+}
