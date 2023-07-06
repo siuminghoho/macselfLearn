160285af45d34ddb2b7d4575528fa272479e033d
felixchan <siuminghoho@hotmail.com>
2023-07-06 18:30:51 +0800

    2nd update gol

--- gol/gameOfLife.js
+++ gol/gameOfLife.js
@@ -1,10 +1,36 @@
+const unitLength = 20;
+const boxColor = 150;
+const strokeColor = 50;
+let columns; /* To be determined by window width */
+let rows; /* To be determined by window height */
+let currentBoard;
+let nextBoard;
+
+
+
+
+
+
 //setup() function is for setting up the initial values. It is going to run exactly once.
 function setup() {
-
-
-
-
-}
+    /* Set the canvas to be under the element #canvas*/
+    const canvas = createCanvas(windowWidth, windowHeight - 100);
+    canvas.parent(document.querySelector("#canvas"));
+  
+    /*Calculate the number of columns and rows */
+    columns = floor(width / unitLength);
+    rows = floor(height / unitLength);
+  
+    /*Making both currentBoard and nextBoard 2-dimensional matrix that has (columns * rows) boxes. */
+    currentBoard = [];
+    nextBoard = [];
+    for (let i = 0; i < columns; i++) {
+      currentBoard[i] = [];
+      nextBoard[i] = [];
+    }
+    // Now both currentBoard and nextBoard are array of array of undefined values.
+    init(); // Set the initial values of the currentBoard and nextBoard
+  }
 
 
 // draw() is going to run many times. It is going to run once per frame. If you have a frame rate of 30 frames per second, then draw() runs 30 times per second!
