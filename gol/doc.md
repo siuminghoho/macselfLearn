e0fd289acdcb51e753592d0c6797955d5f68d5a3
felixchan <siuminghoho@hotmail.com>
2023-07-07 13:59:00 +0800

    start ball

---
+++ gol/doc.md
@@ -0,0 +1,16 @@
+[]-Landing Page for your Game of Life project. It should be a different html file from the Game of life page.
+[]-A simple page to introduce yourself
+Here are some of the topics that you should attempt to work on:
+
+[]-Control speed of the Game of Life. (Checkout framerate(), you can use slider to control the framerate )
+[]-Allow users to change the rules of survival.
+[]-Allow users to change the rules of reproduction.
+[]-Start/Stop the Game of life
+[]-Multiple colors of life on the same board.
+[]-Darken colors for stable life.
+[]-Random initial states
+[]-Well-known patterns of Game of Life to select from (Examples: Gosper Glider Gun, Glider, Lightweight train).
+[]-Use Keyboard to control the cursor to place the life
+[]-Resize board on windows resize (Check out windowsResized())
+[]-Switching between different styles.
+[]-Anything else that you could think of.
--- gol/gameOfLife.js
+++ gol/gameOfLife.js
@@ -10,7 +10,7 @@ let nextBoard;
 
 
 
-
+//p5js function
 //setup() function is for setting up the initial values. It is going to run exactly once.
 function setup() {
     /* Set the canvas to be under the element #canvas*/
@@ -32,13 +32,123 @@ function setup() {
     init(); // Set the initial values of the currentBoard and nextBoard
   }
 
+/**
+ * Initialize/reset the board state
+ */
+function init() {
+    for (let i = 0; i < columns; i++) {
+      for (let j = 0; j < rows; j++) {
+        currentBoard[i][j] = 0;
+        nextBoard[i][j] = 0;
+      }
+    }
+  }
+  
 
+
+//p5js function
 // draw() is going to run many times. It is going to run once per frame. If you have a frame rate of 30 frames per second, then draw() runs 30 times per second!
 
 function draw() {
+    background(255);
+    generate();
+    for (let i = 0; i < columns; i++) {
+      for (let j = 0; j < rows; j++) {
+        if (currentBoard[i][j] == 1) {
+          fill(boxColor);
+        } else {
+          fill(255);
+        }
+        stroke(strokeColor);
+        rect(i * unitLength, j * unitLength, unitLength, unitLength);
+      }
+    }
+  }
+
+//p5js function
+  function generate() {
+    //Loop over every single box on the board
+    for (let x = 0; x < columns; x++) {
+      for (let y = 0; y < rows; y++) {
+        // Count all living members in the Moore neighborhood(8 boxes surrounding)
+        let neighbors = 0;
+        for (let i of [-1, 0, 1]) {
+          for (let j of [-1, 0, 1]) {
+            if (i == 0 && j == 0) {
+              // the cell itself is not its own neighbor
+              continue;
+            }
+            // The modulo operator is crucial for wrapping on the edge
+            neighbors +=
+              currentBoard[(x + i + columns) % columns][(y + j + rows) % rows];
+          }
+        }
+  
+        // Rules of Life
+        if (currentBoard[x][y] == 1 && neighbors < 2) {
+          // Die of Loneliness
+          nextBoard[x][y] = 0;
+        } else if (currentBoard[x][y] == 1 && neighbors > 3) {
+          // Die of Overpopulation
+          nextBoard[x][y] = 0;
+        } else if (currentBoard[x][y] == 0 && neighbors == 3) {
+          // New life due to Reproduction
+          nextBoard[x][y] = 1;
+        } else {
+          // Stasis
+          nextBoard[x][y] = currentBoard[x][y];
+        }
+      }
+    }
+  
+    // Swap the nextBoard to be the current Board
+    [currentBoard, nextBoard] = [nextBoard, currentBoard];
+  }
 
 
 
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * When mouse is dragged
+ */
+function mouseDragged() {
+    /**
+     * If the mouse coordinate is outside the board
+     */
+    if (mouseX > unitLength * columns || mouseY > unitLength * rows) {
+      return;
+    }
+    const x = Math.floor(mouseX / unitLength);
+    const y = Math.floor(mouseY / unitLength);
+    currentBoard[x][y] = 1;
+    fill(boxColor);
+    stroke(strokeColor);
+    rect(x * unitLength, y * unitLength, unitLength, unitLength);
+  }
+  
+  /**
+   * When mouse is pressed
+   */
+  function mousePressed() {
+    noLoop();
+    mouseDragged();
+  }
+  
+  /**
+   * When mouse is released
+   */
+  function mouseReleased() {
+    loop();
+  }
