93b9680a2be3cae6998ed12f3c810d4405b6102d
felixchan <teckycoursefelix@gmail.com>
2023-07-10 09:33:38 +0800

    1111

--- gol/gameOfLife.js
+++ gol/gameOfLife.js
@@ -239,3 +239,16 @@ function handleSpeedChange() {
   frameRate(fr);
 }
 
+
+
+
+// testing
+const heightOutput = document.querySelector("#height");
+const widthOutput = document.querySelector("#width");
+
+function reportWindowSize() {
+  heightOutput.textContent = window.innerHeight;
+  widthOutput.textContent = window.innerWidth;
+}
+
+window.onresize = reportWindowSize;
